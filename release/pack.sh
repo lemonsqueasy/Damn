d5307783053a6f24199aaa6dfbb75db53f191631
lemonsqueasy <59079427+lemonsqueasy@users.noreply.github.com>
2020-03-13 00:27:45 +0800

    调整结构

--- release/pack.sh
+++ release/pack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #不更新库的情况
 cp -rf ../../DamnPack/DamnGame ../../DamnPack/DG/DamnGame.app
-cp -f ../cmake-build-debug/Damn ../../DamnPack/DG/DamnGame.app/Contents/MacOS
+cp -f ../cmake-build-release/Damn ../../DamnPack/DG/DamnGame.app/Contents/MacOS
 cp -rf ../media ../../DamnPack/DG/DamnGame.app/Contents
 hdiutil create -srcfolder ../../DamnPack/DG -volname DamnGame ./DamnGame.dmg
 rm -rf ../../DamnPack/DG/DamnGame.app
